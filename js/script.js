f06c1696f2698c75c9369c298f0c2dd83f673e83
Andrea <andreapirrello1990@gmail.com>
2020-06-15 13:58:26 +0200

    impostato esercizio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// chiedere all'utente il numero di km che vuole percorrere
+
+var km = prompt("inserisci in numero di km che vuoi percorrere:");
+
+// chiedere all'utente la sua età
+
+var eta = prompt("inserisci la tua età:");
+
+// calcolare il prezzo totale del viaggio sapendo che costa 0.21 euro al km
+
+
+
+// applicare uno sconto del 20% per i passeggeri minorenni
+
+// apllicare uno sconto del 40% per i passeggeri over 65
